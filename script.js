5a86752b8537f4344179f9e8b6e76f0319494f2f
Ronihe <herongrong2011@gmail.com>
2018-11-13 21:56:37 -0800

    added the random giphy and error msg feather.

--- script.js
+++ script.js
@@ -4,21 +4,27 @@ $(function(){
     e.preventDefault();
 //get the input  from search 
         let $searchStr = $("#searchStr").val();
-        let giphyUrl = `http://api.giphy.com/v1/gifs/search?q=${$searchStr}&api_key=dc6zaTOxFJmzC`
+        let giphyUrl = `http://api.giphy.com/v1/gifs/random?tag=${$searchStr}&api_key=dc6zaTOxFJmzC`;
 
         $.get(giphyUrl, appendGiphy);
     })
 
     function appendGiphy(res){
-        let giphyPic = res.data[0].images.downsized_large.url;
-        let $img = $("<img class='newGiphy'  width='400'>");
-        $img.attr("src", giphyPic);       
-        $(".container-fluid").append($img);  
-    }
+        $("#warning").empty();
+
+        if(res.data.length === 0){
+            alert("No Giphy, please put in the valid info.");
+        }else{
+            let giphyPic = res.data.images.downsized_large.url;
+            let $img = $("<img class='newGiphy'  width='400'>");
+            $img.attr("src", giphyPic);       
+            $(".container-fluid").append($img);   
+            }
+        }
 
     $("#remove").on("click", function(){
         $(".newGiphy").remove();
-    })
+    });
 
 });
 
